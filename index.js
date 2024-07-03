1144b5f57592ab00ab1acf3ba975053b04141fd7
terenceouma <doros.devs@gmail.com>
2024-07-03 16:02:27 +0300

    first two tests almost fully successful

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickup) {
+    const HQ = 42;
+    return Math.abs(pickup)- HQ;
+
+  }
+
+  function distanceFromHqInFeet(pickup) {
+    const distanceConverter = 264
+  return distanceFromHqInBlocks(pickup) * distanceConverter ;
+  
+}
+
+ function distanceTravelledInFeet(start, destination) {
+    const distanceConverter = 264
+    return (start - destination)* distanceConverter
+  }
