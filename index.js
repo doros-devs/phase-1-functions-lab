3f912569f09cb97e62b7c569afb0c32dbe4f73f2
terenceouma <doros.devs@gmail.com>
2024-07-04 11:02:43 +0300

    done

--- index.js
+++ index.js
@@ -2,17 +2,36 @@
 
 function distanceFromHqInBlocks(pickup) {
     const HQ = 42;
-    return Math.abs(pickup)- HQ;
+    return Math.abs((pickup)- HQ);
 
   }
 
   function distanceFromHqInFeet(pickup) {
-    const distanceConverter = 264
+    const distanceConverter = 264;
   return distanceFromHqInBlocks(pickup) * distanceConverter ;
   
 }
 
  function distanceTravelledInFeet(start, destination) {
-    const distanceConverter = 264
-    return (start - destination)* distanceConverter
+    const distanceConverter = 264;
+    return Math.abs((start - destination)* distanceConverter);
   }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceConverter = 264;
+    const freeDistance = 400;
+    const costPerFeet = 0.02;
+    const distance = Math.abs(start - destination)* distanceConverter;
+
+    if (distance <= freeDistance) {
+        return 0;
+    } else if (distance > freeDistance && distance <= 2000 ) {
+        return (distance - freeDistance) * costPerFeet;
+    }else if(distance > 2000 && distance <= 2500) {
+        return 25;
+    }else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+  }
+
+  
